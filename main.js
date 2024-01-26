930a39e8afdc1fccc3d9e2540f786c0d9fdf1d96
Krishal-Modi <111569458+Krishal-Modi@users.noreply.github.com>
2024-01-26 14:41:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,3 +11,44 @@ document.getElementById('openAndDownloadBtn').addEventListener('click', function
         alert('Popup blocked. Please allow popups to open the PDF.');
     }
 });
+
+
+
+// Carousel
+const carousel = document.getElementById('image-carousel');
+  let currentIndex = 0;
+  let continuousRollCount = 0;
+
+  function showSlide(index) {
+    const newTransformValue = -index * 100 + '%';
+    carousel.style.transform = 'translateX(' + newTransformValue + ')';
+  }
+
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % document.querySelectorAll('.carousel-item').length;
+
+    if (currentIndex === 0) {
+      continuousRollCount++;
+
+      if (continuousRollCount === 2) {
+        // If it has rolled twice, apply the rolling effect
+        carousel.style.transition = 'none';
+        currentIndex = 1; // Set to the second slide instantly
+        showSlide(currentIndex);
+        setTimeout(() => {
+          carousel.style.transition = 'transform 0.5s ease-in-out';
+          currentIndex = 0; // Set back to the first slide
+          showSlide(currentIndex);
+        }, 0);
+        continuousRollCount = 0; // Reset the continuous roll count
+      }
+    } else {
+      showSlide(currentIndex);
+    }
+  }
+
+  function prevSlide() {
+    currentIndex = (currentIndex - 1 + document.querySelectorAll('.carousel-item').length) % document.querySelectorAll('.carousel-item').length;
+    continuousRollCount = 0; // Reset the continuous roll count when going back to previous slides
+    showSlide(currentIndex);
+  }
